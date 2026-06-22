03ec6547f6e35740c217a51c7806c907edcd2fd0
Ulug'bek <ulugbekrahmatullayev28gmail.com@GLHF.local>
2026-06-22 15:25:08 +0500

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import RealCosmos from './pages/RealCosmos'
 import Sandbox from './pages/Sandbox'
 
 export default function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/real-cosmos" element={<RealCosmos />} />
         <Route path="/sandbox" element={<Sandbox />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
